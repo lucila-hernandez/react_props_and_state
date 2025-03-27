371a128d80bf2032f7b85b40eea11038c8544f94
lucila-hernandez <lucila.hernandezdelcarmen@students.dominican.edu>
2025-03-27 14:51:00 -0700

    completed Part 1: Creating a Counter Component(Props)

---
+++ lesson_2/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './Counter'
+
+function App() {
+  return (
+    <div className='App'>
+      <Counter label="Apples" value={1} />
+      <Counter label="Oranges" value={3} />
+    </div>
+  );
+}
+
+export default App;
---
+++ lesson_2/src/Counter.jsx
@@ -0,0 +1,13 @@
+
+function Counter(props) {
+    return (
+        <div className="Counter">
+            <small>{props.label}</small>
+            <h1>{props.value}</h1>
+            <button>+</button>
+            <button>-</button>
+        </div>
+    )
+}
+
+export default Counter;
